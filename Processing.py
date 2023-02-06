d0bcdb88163154db14241f38d71811a83e170808
Deep <shuvodipsaha74@gmail.com>
2023-02-06 10:35:32 +0530

    First Commit

--- Processing.py
+++ Processing.py
@@ -1,5 +1,26 @@
 import os
+import pandas as pd
 
+print(os.getcwd())
 files = os.listdir()
+set = {}
+for file in files:
 
-print(files)
+
+    if file == 'csv':
+
+        new_path = os.path.join(os.getcwd(),file)
+
+
+        for dataset in os.listdir(new_path):
+
+            head,sep,tail = dataset.partition('.')
+            #print(head)
+            data = pd.read_csv(os.path.join(new_path,dataset))
+            set[head] = data
+print(set)
+
+
+
+
+
